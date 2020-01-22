ed0bfbf2b3e6a85886e5664479250871bbab5993
BlackZan1 <kenshin.nakamura.01@mail.ru>
2020-01-22 21:59:42 +0600

    Updates

---
+++ precache-manifest.fc4b254a46b2f5bae96903ccf6e55a0a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726049489376c5d32d8d54c0268a1bec",
+    "url": "/react-crm-app/index.html"
+  },
+  {
+    "revision": "8393c22e361ccb7193c3",
+    "url": "/react-crm-app/static/css/main.3e7cf5c1.chunk.css"
+  },
+  {
+    "revision": "dff9fec944871b091edf",
+    "url": "/react-crm-app/static/js/2.d920a362.chunk.js"
+  },
+  {
+    "revision": "d34a11d4109fc2788407628ee7dce806",
+    "url": "/react-crm-app/static/js/2.d920a362.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8393c22e361ccb7193c3",
+    "url": "/react-crm-app/static/js/main.bb0e006e.chunk.js"
+  },
+  {
+    "revision": "549047c4c3252a27a0ca",
+    "url": "/react-crm-app/static/js/runtime-main.26ac88b0.js"
+  },
+  {
+    "revision": "942e123b8a9d5f545c6fccca5f4e8541",
+    "url": "/react-crm-app/static/media/add.942e123b.png"
+  },
+  {
+    "revision": "a3b868b1b44d525cdd1bbe69bdb03778",
+    "url": "/react-crm-app/static/media/budget.a3b868b1.png"
+  },
+  {
+    "revision": "9e6758379785df96c48d77fae894f0e9",
+    "url": "/react-crm-app/static/media/business.9e675837.png"
+  },
+  {
+    "revision": "de26ee5cd70c774c7c59f3a73303bacf",
+    "url": "/react-crm-app/static/media/history.de26ee5c.png"
+  },
+  {
+    "revision": "46a54e54a795936394436e49c4d7d7f5",
+    "url": "/react-crm-app/static/media/loader.46a54e54.gif"
+  },
+  {
+    "revision": "f728a4124ffe400142e2241c4e0ef1df",
+    "url": "/react-crm-app/static/media/user.f728a412.png"
+  }
+]);
