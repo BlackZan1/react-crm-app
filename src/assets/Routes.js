803807b9c110ac0f24614c42e4617970d6085877
BlackZan1 <kenshin.nakamura.01@mail.ru>
2020-01-22 22:07:48 +0600

    routes fix

--- src/assets/Routes.js
+++ src/assets/Routes.js
@@ -4,23 +4,25 @@ import HomePage from '../components/HomePage/index';
 import BudgetPage from '../components/BudgetPage/index';
 import BusinessPage from '../components/BusinessPage/index.tsx';
 
+const BASE_URL = '/react-crm-app';
+
 export const useRoutes = (isAuth) => {
     if(isAuth) {
         return (
             <Switch>
-                <Route exact path={'/home'}>
+                <Route exact path={`${BASE_URL}/home`}>
                     <HomePage />
                 </Route>
 
-                <Route exact path={'/budget'}>
+                <Route exact path={`${BASE_URL}/budget`}>
                     <BudgetPage />
                 </Route>
 
-                <Route exact path={'/business'}>
+                <Route exact path={`${BASE_URL}/business`}>
                     <BusinessPage />
                 </Route>
 
-                <Redirect to={'/home'} />
+                <Redirect to={`${BASE_URL}/home`} />
             </Switch>
         )
     }
